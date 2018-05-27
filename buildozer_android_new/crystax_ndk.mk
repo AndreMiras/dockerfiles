d79c4c372fab5e87615cb5d9b8bc6be6bfef2047
Andre Miras <andre.miras@gmail.com>
2018-05-28 01:04:58 +0200

    Configurable tar command binary
    So it can be replaced by `bsdtar`.
    On Travis some build may fail with:
    ```
    tar -xf crystax-ndk-10.3.2-linux-x86_64.tar.xz --directory /home/user/.android \
        --exclude=crystax-ndk-10.3.2/docs \
        --exclude=crystax-ndk-10.3.2/samples \
        --exclude=crystax-ndk-10.3.2/tests \
        --exclude=crystax-ndk-10.3.2/toolchains/renderscript \
        --exclude=crystax-ndk-10.3.2/toolchains/x86_64-* \
        --exclude=crystax-ndk-10.3.2/toolchains/llvm-* \
        --exclude=crystax-ndk-10.3.2/toolchains/aarch64-* \
        --exclude=crystax-ndk-10.3.2/toolchains/mips64el-*
    tar: crystax-ndk-10.3.2/sources/cpufeatures: Directory renamed before its status could be extracted
    tar: Exiting with failure status due to previous errors
    make: *** [extract_crystax_ndk] Error 2
    crystax_ndk.mk:20: recipe for target 'extract_crystax_ndk' failed
    The command '/bin/sh -c make -f crystax_ndk.mk' returned a non-zero code: 2
    ```

--- buildozer_android_new/crystax_ndk.mk
+++ buildozer_android_new/crystax_ndk.mk
@@ -8,6 +8,7 @@ CRYSTAX_NDK_HOME=$(ANDROID_HOME)/crystax-ndk
 CRYSTAX_NDK_HOME_V=$(CRYSTAX_NDK_HOME)-$(CRYSTAX_NDK_VERSION)
 CRYSTAX_NDK_ARCHIVE=crystax-ndk-$(CRYSTAX_NDK_VERSION)-linux-x86_64.tar.xz
 CRYSTAX_NDK_DL_URL=https://eu.crystax.net/download/$(CRYSTAX_NDK_ARCHIVE)
+TAR=`which tar`
 
 
 all: download_crystax_ndk extract_crystax_ndk
@@ -18,7 +19,7 @@ download_crystax_ndk:
 
 extract_crystax_ndk:
 	mkdir -p $(ANDROID_HOME)
-	tar -xf $(CRYSTAX_NDK_ARCHIVE) --directory $(ANDROID_HOME) \
+	$(TAR) -xf $(CRYSTAX_NDK_ARCHIVE) --directory $(ANDROID_HOME) \
 	    --exclude=crystax-ndk-$(CRYSTAX_NDK_VERSION)/docs \
 	    --exclude=crystax-ndk-$(CRYSTAX_NDK_VERSION)/samples \
 	    --exclude=crystax-ndk-$(CRYSTAX_NDK_VERSION)/tests \
